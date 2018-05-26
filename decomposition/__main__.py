389c29182d94c5e733912c01ddc8b7072c02ceb6
Leonardo Jose  Schettini de Arruda <leoschettini2@gmail.com>
2018-05-25 23:34:54 -0300

    Add LDA to main script

--- decomposition/__main__.py
+++ decomposition/__main__.py
@@ -1,8 +1,8 @@
 import codecs
 
-from decomposition import PrincipalComponentAnalysis
+from decomposition import PrincipalComponentAnalysis, LinearDiscriminantAnalysis
 from retriever.data_collection import DataCollection, EDataType
-
+from plot import plot_stuff
 from sklearn.metrics import precision_recall_fscore_support
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import KFold
@@ -12,11 +12,18 @@ print('Collecting data...')
 data_collection = DataCollection('res/promise/')
 
 pca = PrincipalComponentAnalysis()
+lda = LinearDiscriminantAnalysis()
+
 knn = KNeighborsClassifier(n_neighbors=9, weights='distance', algorithm='auto')
 kf = KFold(n_splits=5, shuffle=True) # 80% for training, 20% for testing
 
+results = {}
 for relation in data_collection.documents:
     file_ = codecs.open('res/results/' + relation + '.txt', 'w+', 'utf-8')
+
+    results[relation] = {}
+    results[relation]['PCA'] = {}
+    results[relation]['LDA'] = {}
 
     print('\nTesting for ' + relation + ' data set')
     relation_data, relation_labels = data_collection.get_data_label(relation)
@@ -29,31 +36,66 @@ for relation in data_collection.documents:
     features_types = data_collection.get_features_types(relation)
 
     pca.fit(relation_data)
+    lda.fit(relation_data, relation_labels)
 
-    precision_mean = []
-    recall_mean = []
-    f1_mean = []
+    pca_precision_mean = []
+    pca_recall_mean = []
+    pca_f1_mean = []
+
+    lda_precision_mean = []
+    lda_recall_mean = []
+    lda_f1_mean = []
     for n_components in range(1, features_amount+1):
-        data_components = pca.transform(relation_data, n_components)
+        pca_components = pca.transform(relation_data, n_components)
+        lda_components = lda.transform(relation_data, n_components)
 
-        metrics = []
-        for train_indexes, test_indexes in kf.split(data_components):
-            train_data, test_data = data_components[train_indexes], data_components[test_indexes]
+        pca_metrics = []
+        lda_metrics = []
+        for train_indexes, test_indexes in kf.split(relation_data):
+            pca_train, pca_test = pca_components[train_indexes], pca_components[test_indexes]
+            lda_train, lda_test = lda_components[train_indexes], lda_components[test_indexes]
             train_labels, test_labels = relation_labels[train_indexes], relation_labels[test_indexes]
 
-            knn.fit(train_data, train_labels)
-            pred_labels = knn.predict(test_data)
+            # PCA testing
+            knn.fit(pca_train, train_labels)
+            pred_labels = knn.predict(pca_test)
+            pca_metrics.append(precision_recall_fscore_support(test_labels, pred_labels, average='weighted'))
 
-            metrics.append(precision_recall_fscore_support(test_labels, pred_labels, average='weighted'))
+            # LDA testing
+            knn.fit(lda_train, train_labels)
+            pred_labels = knn.predict(lda_test)
+            lda_metrics.append(precision_recall_fscore_support(test_labels, pred_labels, average='weighted'))
         
-        precisions = [precision for precision, _, _, _ in metrics]
-        recalls = [recall for _, recall, _, _ in metrics]
-        f1s = [f1 for _, _, f1, _ in metrics]
-        
-        precision_mean.append(np.mean(precisions))
-        recall_mean.append(np.mean(recalls))
-        f1_mean.append(np.mean(f1s))
+        pca_precisions = [precision for precision, _, _, _ in pca_metrics]
+        pca_recalls = [recall for _, recall, _, _ in pca_metrics]
+        pca_f1s = [f1 for _, _, f1, _ in pca_metrics]
 
-    file_.write('precision:\n' + str(precision_mean))
-    file_.write('\n\nrecall:\n' + str(recall_mean))
-    file_.write('\n\nf1:\n' + str(f1_mean))
+        lda_precisions = [precision for precision, _, _, _ in lda_metrics]
+        lda_recalls = [recall for _, recall, _, _ in lda_metrics]
+        lda_f1s = [f1 for _, _, f1, _ in lda_metrics]
+        
+        pca_precision_mean.append(np.mean(pca_precisions))
+        pca_recall_mean.append(np.mean(pca_recalls))
+        pca_f1_mean.append(np.mean(pca_f1s))
+
+        lda_precision_mean.append(np.mean(lda_precisions))
+        lda_recall_mean.append(np.mean(lda_recalls))
+        lda_f1_mean.append(np.mean(lda_f1s))
+
+        results[relation]['PCA']['precision'] = pca_precision_mean
+        results[relation]['PCA']['recall'] = pca_recall_mean
+        results[relation]['PCA']['f1'] = pca_f1_mean
+
+        results[relation]['LDA']['precision'] = lda_precision_mean
+        results[relation]['LDA']['recall'] = lda_recall_mean
+        results[relation]['LDA']['f1'] = lda_f1_mean
+
+    file_.write('PCA precision:\n' + str(pca_precision_mean))
+    file_.write('\n\nPCA recall:\n' + str(pca_recall_mean))
+    file_.write('\n\nPCA f1:\n' + str(pca_f1_mean))
+
+    file_.write('\n\nLDA precision:\n' + str(lda_precision_mean))
+    file_.write('\n\nLDA recall:\n' + str(lda_recall_mean))
+    file_.write('\n\nLDA f1:\n' + str(lda_f1_mean))
+
+    plot_stuff(results[relation], relation, list(range(1, features_amount+1)))
