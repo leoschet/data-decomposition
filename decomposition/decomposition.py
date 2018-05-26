ad02c350a87d5dffdea03db3bb49575df4e80515
Leonardo Jose  Schettini de Arruda <leoschettini2@gmail.com>
2018-05-25 23:35:03 -0300

    Implement LDA

--- decomposition/decomposition.py
+++ decomposition/decomposition.py
@@ -1,7 +1,23 @@
 import numpy as np
 import operator
 
-class PrincipalComponentAnalysis(object):
+
+class Decomposition(object):
+    def transform(self, dataset, n_components):
+        dataset_len = len(dataset)
+
+        # Create transformer matrix
+        transformer = self.sorted_eigenvecs[0]
+        for i in range(1, n_components):
+            transformer = np.vstack((transformer, self.sorted_eigenvecs[i]))
+
+        # Project the dataset on the new system
+        transformed = transformer.dot(dataset.T)
+
+        transformed.shape = (dataset_len, n_components)
+        return transformed
+
+class PrincipalComponentAnalysis(Decomposition):
     def fit(self, dataset):
         # Calculate the mean of each feature
         features_mean = list(map(np.mean, dataset.T))
@@ -22,18 +38,40 @@ class PrincipalComponentAnalysis(object):
         # Sort elements from biggest eigenvalue to smallest
         eigen.sort(key=lambda x: x[0], reverse=True)
 
-        self.eigenval, self.sorted_eigenvecs = tuple(zip(*eigen))
+        _, self.sorted_eigenvecs = tuple(zip(*eigen))
 
-    def transform(self, dataset, n_components):
-        dataset_len = len(dataset)
+class LinearDiscriminantAnalysis(Decomposition):
+    def fit(self, dataset, labels):
+        global_means = np.array(list(map(np.mean, dataset.T)))
+        between_matrix = 0
+        within_matrix = 0
 
-        # Create transformer matrix
-        transformer = self.sorted_eigenvecs[0]
-        for i in range(1, n_components):
-            transformer = np.vstack((transformer, self.sorted_eigenvecs[i]))
+        unique_labels = set(labels)
+        for label in unique_labels:
+            # Separate data by label and get amount of data
+            indices = np.where(labels == label)[0]
+            data_amount = indices.size
 
-        # Project the dataset on the new system
-        transformed = transformer.dot(dataset.T)
+            # Calculate the mean of each feature for every label (between matrix step)
+            means = np.array(list(map(np.mean, dataset[indices].T)))
 
-        transformed.shape = (dataset_len, n_components)
-        return transformed
+            # Normalize by the global means (between matrix step)
+            means -= global_means
+
+            between_matrix += data_amount * means.dot(means.T)
+            within_matrix += np.cov(dataset.T)
+
+        # Calculate the inverse of the within matrix
+        inverse_within = np.linalg.inv(within_matrix)
+
+        # Eigenvectors and eigenvalues from the covariance matrix
+        # The ith eigenvalue corresponde to the ith column vector
+        eigenval, eigenvecs = np.linalg.eig(inverse_within.dot(between_matrix))
+
+        # Transform list of list [vals, vecs] to list of tuple (val, vec)
+        eigen = list(zip(eigenval, eigenvecs.T))
+
+        # Sort elements from biggest eigenvalue to smallest
+        eigen.sort(key=lambda x: x[0], reverse=True)
+
+        _, self.sorted_eigenvecs = tuple(zip(*eigen))
